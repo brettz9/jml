6924cf7c018e8c0cc464e2c15070f48b2c8671b5
Brett Zamir <brettz9@yahoo.com>
2013-08-01 11:15:19 +0800

    coding standards

--- shims/Array.prototype.forEach.js
+++ shims/Array.prototype.forEach.js
@@ -1,13 +1,14 @@
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 // Production steps of ECMA-262, Edition 5, 15.4.4.18
 // Reference: http://es5.github.com/#x15.4.4.18
-if ( !Array.prototype.forEach ) {
+if (!Array.prototype.forEach) {
 
-  Array.prototype.forEach = function forEach( callback, thisArg ) {
+  Array.prototype.forEach = function forEach(callback, thisArg) {
     'use strict';
     var T, k;
 
     if (this == null) {
-      throw new TypeError( "this is null or not defined" );
+      throw new TypeError("this is null or not defined");
     }
 
     var kValue,
@@ -20,12 +21,12 @@ if ( !Array.prototype.forEach ) {
 
     // 4. If IsCallable(callback) is false, throw a TypeError exception.
     // See: http://es5.github.com/#x9.11
-    if ( {}.toString.call(callback) !== "[object Function]" ) {
-      throw new TypeError( callback + " is not a function" );
+    if ({}.toString.call(callback) !== "[object Function]") {
+      throw new TypeError(callback + " is not a function");
     }
 
     // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-    if ( arguments.length >= 2 ) {
+    if (arguments.length >= 2) {
       T = thisArg;
     }
 
@@ -43,11 +44,11 @@ if ( !Array.prototype.forEach ) {
       if (k in O) {
 
         // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-        kValue = O[ k ];
+        kValue = O[k];
 
         // ii. Call the Call internal method of callback with T as the this value and
         // argument list containing kValue, k, and O.
-        callback.call( T, kValue, k, O );
+        callback.call(T, kValue, k, O);
       }
       // d. Increase k by 1.
       k++;
